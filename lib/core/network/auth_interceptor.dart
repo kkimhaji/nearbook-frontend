d4e04f662e3d167c536f8acf30aa04d2135dfb09
kkimhaji <kkim.haji@gmail.com>
2026-03-31 18:03:31 +0900

    [feat] add ApiConstants, SecureStorage, and DioClient with AuthInterceptor

---
+++ lib/core/network/auth_interceptor.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import '../storage/secure_storage.dart';
+
+class AuthInterceptor extends Interceptor {
+  @override
+  Future<void> onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    final token = await SecureStorage.getToken();
+    if (token != null) {
+      options.headers['Authorization'] = 'Bearer $token';
+    }
+    handler.next(options);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    // 401 시 로그아웃 처리는 추후 auth provider에서 처리
+    handler.next(err);
+  }
+}
---
+++ lib/core/network/dio_client.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+import '../constants/api_constants.dart';
+import 'auth_interceptor.dart';
+
+class DioClient {
+  static Dio? _instance;
+
+  static Dio get instance {
+    _instance ??= _createDio();
+    return _instance!;
+  }
+
+  static Dio _createDio() {
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: ApiConstants.baseUrl,
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 10),
+        headers: {'Content-Type': 'application/json'},
+      ),
+    );
+
+    dio.interceptors.add(AuthInterceptor());
+
+    return dio;
+  }
+}
