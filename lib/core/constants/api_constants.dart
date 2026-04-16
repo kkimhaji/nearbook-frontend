b29f3efa6d360333d47a77fdde3127cf4b957b9a
kkimhaji <kkim.haji@gmail.com>
2026-04-16 16:15:47 +0900

    [refactor] load backend endpoints from dart-define for real-device development

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,4 +1,6 @@
 class ApiConstants {
-  static const String baseUrl = 'http://localhost:3000/api';
-  static const String socketUrl = 'http://localhost:3000';
+  static const String baseUrl = String.fromEnvironment('BASE_URL',
+      defaultValue: 'http://localhost:3000/api');
+  static const String socketUrl = String.fromEnvironment('SOCKET_URL',
+      defaultValue: 'http://localhost:3000');
 }
